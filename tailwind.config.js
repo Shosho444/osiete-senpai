cc1dfa2de5b88bde65c4a9f935984457e0a8b163
芝咲昇誤 <shougosbzk@gmail.com>
2024-01-06 02:18:06 +0900

    変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
@@ -5,5 +7,15 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          500: "#ef4444",
+        },
+        primary: colors.forest,
+      },
+    },
+  },
   plugins: [require("daisyui")]
 }
